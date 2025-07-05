c8deb9260df65569079a19c0fd9232adcb04df02
vasilisnikitaras <vasilis.nikitaras@gmail.com>
2025-07-05 10:42:07 -0400

    Create lang.js

---
+++ daily-assist/lang.js
@@ -0,0 +1,14 @@
+const translations = {
+  en: {
+    title: "📝 Daily Assist",
+    todo: "To-Do List",
+    journal: "Journal",
+    placeholder: "Start writing...",
+  },
+  el: {
+    title: "📝 Καθημερινή Βοήθεια",
+    todo: "Λίστα Εργασιών",
+    journal: "Ημερολόγιο",
+    placeholder: "Σημείωσε τις σκέψεις σου...",
+  },
+};
